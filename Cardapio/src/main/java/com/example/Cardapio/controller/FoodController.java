eb6b5c2402aaf602d4bd18f6b4ca0ec78865fe6a
Bruno Luis <165501730+BrunoLUFPB@users.noreply.github.com>
2025-04-14 06:19:00 -0300

    Alteração no FoodController

--- Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
+++ Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
@@ -30,4 +30,27 @@ public class FoodController {
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/{id}")
+    public FoodResponseDTO getById(@PathVariable Long id) {
+        Food food = repository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+        return new FoodResponseDTO(food);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping("/{id}")
+    public void updateFood(@PathVariable Long id, @RequestBody FoodResquestDTO data) {
+        Food food = repository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+        food.setTitle(data.title());
+        food.setImage(data.image());
+        food.setPrice(data.price());
+        repository.save(food);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public void deleteFood(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
