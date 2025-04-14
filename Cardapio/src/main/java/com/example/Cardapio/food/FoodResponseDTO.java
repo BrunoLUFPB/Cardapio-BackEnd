d613e4a932b46c660ec8fc2152d68850b3657345
Bruno Luis <165501730+BrunoLUFPB@users.noreply.github.com>
2025-04-14 06:40:51 -0300

    Pequena alteração na classe FoodResponseDTO

--- Cardapio/src/main/java/com/example/Cardapio/food/FoodResponseDTO.java
+++ Cardapio/src/main/java/com/example/Cardapio/food/FoodResponseDTO.java
@@ -1,6 +1,6 @@
 package com.example.Cardapio.food;
 
-public record FoodResponseDTO(Long id, String name, String image, Integer price, String description) {
+public record FoodResponseDTO(Long id, String title, String image, Integer price, String description) {
     public FoodResponseDTO(Food food) {
         this(food.getId(), food.getTitle(), food.getImage(), food.getPrice(), food.getDescription());
     }
